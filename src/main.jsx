7f78c08093f090a73eefffcc11703e3f294d76ab
dharagrid <sureshjogur@dharagrid.com>
2025-09-20 00:19:51 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import LandingPage from './App.jsx'
+import LandingPage from './LandingPage.jsx'
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <LandingPage />
-  </React.StrictMode>,
+  </React.StrictMode>
 )
+
